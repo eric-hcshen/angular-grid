d9603c53e41f19d5f3266695f035217cbeadcdbe
eric-hcshen <eric.hcshen@gmail.com>
2019-11-18 08:32:51 +0800

    mater-detail

--- src/app/md/md.component.ts
+++ src/app/md/md.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Module, AllModules } from '@ag-grid-enterprise/all-modules';
-//import { AgGridAngular } from '@ag-grid-community/angular';
+import { AgGridAngular } from '@ag-grid-community/angular';
 @Component({
   selector: 'md',
   templateUrl: './md.component.html',
@@ -9,7 +9,7 @@ import { Module, AllModules } from '@ag-grid-enterprise/all-modules';
 })
 
 export class MdComponent {
-  //@ViewChild('myGrid', {static: true}) grid: AgGridAngular;
+  @ViewChild('agGrid', {static: true}) grid: AgGridAngular;
   private gridApi;
   private gridColumnApi;
   public modules = AllModules;
@@ -22,12 +22,13 @@ export class MdComponent {
     this.columnDefs = [
       {
         field: 'master_name',
-        cellRenderer: 'agGroupCellRenderer'
+        cellRenderer: 'agGroupCellRenderer',
+        editable: true
       },
-      { field: "col1" },
-      { field: "col2" },
+      { field: "col1", editable: true },
+      { field: "col2", editable: true },
       {
-        field: "col3",
+        field: "col3", editable: true
         //valueFormatter: "x.toLocaleString() + 'm'"
       }
     ];
@@ -52,14 +53,32 @@ export class MdComponent {
       }
     };
   }
-
+  onAdd(event) {
+    const headers = new HttpHeaders()
+    .set("Content-Type", "application/json");
+    this.http.post('http://localhost:3000/masters',
+    {master_name: 'XXX', col1: 'test1', col2: 'test2', col3: 'test3'},
+    {headers}).subscribe(response => {
+      console.log(response);
+    });
+    this.refresh();
+  }
   onFirstDataRendered(params) {
     params.api.sizeColumnsToFit();
     setTimeout(function() {
       params.api.getDisplayedRowAtIndex(1).setExpanded(true);
     }, 0);
   }
-
+  refresh () {
+    this.http
+    .get(
+      //"https://raw.githubusercontent.com/ag-grid/ag-grid-docs/latest/src/javascript-grid-master-detail/simple/data/data.json"
+        'http://localhost:3000/masters?filter[include][][relation]=details'
+      )
+    .subscribe(data => {
+      this.rowData = data;
+    });
+  }
   onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
@@ -72,9 +91,23 @@ export class MdComponent {
       .subscribe(data => {
         this.rowData = data;
       });
-      /*
       this.grid.gridOptions.onCellEditingStopped = (event) => {
+        console.log(event.value + event.colDef.field + event.data.id);
+        console.log(`http://localhost:3000/masters/{${event.data.id}}`);
+        console.log(`{${event.colDef.field}=${event.value}}`);
         console.log(event);
-      };*/
+        const headers = new HttpHeaders().set('Content-Type', 'application/json');
+        const params = new HttpParams({
+          fromObject: { master_name: 'test02'} //<==not work
+          });
+        this.http.patch(`http://localhost:3000/masters/${event.data.id}`,
+         //`"{"${event.colDef.field}"="${event.value}"}"`
+         //params
+         event.data
+         , {headers})
+         .subscribe(data => {
+           console.log(data); //`${event.colDef.field}=${event.value}`
+         });
+      };
   }
 }
